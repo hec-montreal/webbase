8820a572dffecb6d332f517b6b3d3d68a0d3e3a3
simonggauthierhec <simon.gregoire-gauthier@hec.ca>
2016-12-02 10:39:57 -0500

    Base jsp view

--- src/main/java/ca/hec/webbase/controller/IndexController.java
+++ src/main/java/ca/hec/webbase/controller/IndexController.java
@@ -3,14 +3,14 @@ package ca.hec.webbase.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController
 {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public @ResponseBody String getIndex()
+	public ModelAndView getIndex()
 	{
-		return "Hello, world!";
+		return new ModelAndView("index.jsp");
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8"%>
+
+<%@ include file="/WEB-INF/jsp/include/include.jsp"%>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		
+	</head>
+	
+	<body>
+		<h1>Hello, World!</h1>
+	</body>	
+</html>
